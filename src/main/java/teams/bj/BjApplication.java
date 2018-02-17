5aa980028c61a3498f7dccdf9fca41c3bea5588b
Sophie Jackson-Lee <sophiejacksonlee@sophie-mb.mynet>
2018-02-17 20:18:54 +0000

    First commit - project created: SJL

---
+++ src/main/java/teams/bj/BjApplication.java
@@ -0,0 +1,12 @@
+package teams.bj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjApplication.class, args);
+	}
+}
